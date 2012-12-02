3cb525f47f4680a14d3b90e2c77ef365a0761b8f
wmadruga <willpaixao@gmail.com>
2012-12-02 20:53:45 -0200

    More fields to the form

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -5,7 +5,10 @@ $(function(){
 	
 		// Masked Inputs config
 		$('#nascimento').mask("99/99/9999");
-	
+		$('#cep').mask("99999-999");
+		$('#telefone').mask("(999) 9999-9999");
+		$('#celular').mask("(999) 9999-9999");
+		
 		// Save button
 		$('#save.aluno').on('click', function(){
 			$.post("/aluno/save", $("#alunoForm").serialize(), function(data) {
