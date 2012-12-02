00a00e907f7d49afa60b5eb8224830a542acdcc1
wmadruga <willpaixao@gmail.com>
2012-12-02 21:37:34 -0200

    More fields and logics

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -8,7 +8,20 @@ $(function(){
 		$('#cep').mask("99999-999");
 		$('#telefone').mask("(999) 9999-9999");
 		$('#celular').mask("(999) 9999-9999");
+		$('#conjugueCel').mask("(999) 9999-9999");
 		
+		// Show/Hide
+		$('#estadoCivil').on('change', function() {
+			if($('#estadoCivil').val() == 'casado') {
+			$('.control-group.conjuge').show('fast');
+			$('.control-group.conjuge').show('fast');
+			}
+			else {
+				$('.control-group.conjuge').hide('fast');
+				$('.control-group.conjuge').hide('fast');
+			}
+		});
+
 		// Save button
 		$('#save.aluno').on('click', function(){
 			$.post("/aluno/save", $("#alunoForm").serialize(), function(data) {
