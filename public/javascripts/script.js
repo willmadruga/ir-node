475b521cd2336f920395c3cd8c3f1e2ae43143cd
wmadruga <willpaixao@gmail.com>
2012-12-03 03:10:42 -0200

    More fields on Aluno

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -5,19 +5,40 @@ $(function(){
 	
 		// Masked Inputs config
 		$('#nascimento').mask("99/99/9999");
+		$('#diaVencimento').mask("99");
 		$('#cep').mask("99999-999");
 		$('#telefone').mask("(999) 9999-9999");
 		$('#celular').mask("(999) 9999-9999");
 		$('#conjugueCel').mask("(999) 9999-9999");
+		$('#paiCel').mask("(999) 9999-9999");
+		$('#maeCel').mask("(999) 9999-9999");
 		
 		// Show/Hide
-		$('#estadoCivil').on('change', function() {
+		if($('#estadoCivil').val() == '') {
 			if($('#estadoCivil').val() == 'casado') {
-			$('.control-group.conjuge').show('fast');
-			$('.control-group.conjuge').show('fast');
+				$('.control-group.conjuge').show('fast');
+				$('.control-group.parents').hide('fast');
+			}
+			else if($('#estadoCivil').val() == 'solteiro') {
+				$('.control-group.parents').show('fast');
+				$('.control-group.conjuge').hide('fast');
 			}
 			else {
+				$('.control-group.parents').hide('fast');
 				$('.control-group.conjuge').hide('fast');
+			}
+		}
+		$('#estadoCivil').on('change', function() {
+			if($('#estadoCivil').val() == 'casado') {
+				$('.control-group.conjuge').show('fast');
+				$('.control-group.parents').hide('fast');
+			}
+			else if($('#estadoCivil').val() == 'solteiro') {
+				$('.control-group.parents').show('fast');
+				$('.control-group.conjuge').hide('fast');
+			}
+			else {
+				$('.control-group.parents').hide('fast');
 				$('.control-group.conjuge').hide('fast');
 			}
 		});
